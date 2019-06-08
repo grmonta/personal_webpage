82fdfed03a15973c5180952b9b631f7e0b89ffba
Grmonta <grmonta@gmail.com>
2019-06-08 12:15:12 -0500

    added in easy drinking project

--- html_form_send.php
+++ html_form_send.php
@@ -2,7 +2,7 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "giovan@giovanmontanaro.com";
+$recipient = "gmontanaro@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
