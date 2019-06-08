a321fb9fa0481c4649510bfab4d4ad223e3ab5a9
Grmonta <grmonta@gmail.com>
2019-06-08 11:21:58 -0500

    styling and content updates

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function() {
   mainNav.classList.toggle('active');
 });
+
+function closeUp() {
+  mainNav.classList.toggle('active');
+}
